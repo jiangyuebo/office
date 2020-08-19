edd9d45d20757e625ab519b8c18f8d1232929d53
jiangyuebo <jiangyuebo@gmail.com>
2020-08-18 23:26:05 -0700

    delete

--- office/settings.py
+++ office/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*4ak^zu#-pa9nra@82x)rg7j)ktkh6sm1w$+p$1w**mi2n3^h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
